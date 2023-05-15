159acf4ea3f7b876e71d4e7af5898c2d6c3dc85e
Timhamer <240159@edu.rocfriesepoort.nl>
2023-05-15 15:23:16 +0200

    Update UserClass

---
+++ Classes/AdminClass.php
@@ -0,0 +1,17 @@
+<?php
+include 'UserClass.php';
+class Admins extends Users
+{
+    public string $password;
+
+    public function login()
+    {
+        
+    }
+
+    public function logout() 
+    {
+
+    }
+
+}
