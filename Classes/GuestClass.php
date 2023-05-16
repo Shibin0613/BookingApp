d3fefad090f495e795196046a6ecd2ad6dd4e93f
ShibinPan <220617@edu.rocfriesepoort.nl>
2023-05-16 13:55:16 +0200

    booking api

--- Classes/GuestClass.php
+++ Classes/GuestClass.php
@@ -1,5 +1,7 @@
 <?php
 include 'UserClass.php';
+use Controllers\DB;
+
 class Guests extends Users
 {
     public string $postalCode;
@@ -7,7 +9,17 @@ class Guests extends Users
 
     public function createGuest() 
     {
-        
+        $postalCode = $_POST['postcode'];
+        $houseNumber = $_POST['huisnummer'];
+
+        $guesttable = "guests";
+        $guestdata = [
+            'postalcode' -> $postalCode,
+            'housenumber' -> $houseNumber,
+        ];
+
+        $createguest = DB::insert($guesttable, $guestdata,'Guests');
+        return $createguest;
     }
 
     public function readGuest($id)
