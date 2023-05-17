c42bd9c904032715ecb1a18de6adfbe09ca84a3c
kevinka1 <kevinka1239@gmail.com>
2023-05-17 13:29:49 +0200

    push

---
+++ Pagina/login.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="nl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login.html</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<style>
+    /* login pagina */
+
+    .gradient-custom-2 {
+        /* fallback for old browsers */
+        background: #fccb90;
+
+        /* Chrome 10-25, Safari 5.1-6 */
+        background: -webkit-linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593);
+
+        /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+        background: linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593);
+    }
+
+    @media (min-width: 768px) {
+        .gradient-form {
+            height: 100vh !important;
+        }
+    }
+
+    @media (min-width: 769px) {
+        .gradient-custom-2 {
+            border-top-right-radius: .3rem;
+            border-bottom-right-radius: .3rem;
+        }
+    }
+</style>
+
+<body>
+    <form method="post" action="">
+        <section class="h-100 gradient-form" style="background-color: #eee;">
+            <div class="container py-5 h-100">
+                <div class="row d-flex justify-content-center align-items-center h-100">
+                    <div class="col-xl-10">
+                        <div class="card rounded-3 text-black">
+                            <div class="row g-0">
+                                <div class="col-lg-6">
+                                    <div class="card-body p-md-5 mx-md-4">
+
+                                        <div class="text-center">
+                                            <h4 class="mt-1 mb-5 pb-1">booking.com</h4>
+                                        </div>
+
+                                        <form>
+                                            <p>log hier in</p>
+
+                                            <div class="form-outline mb-4">
+                                                <input type="email" name="email" class="form-control form-control-lg" placeholder="Enter a valid email address" />
+                                                <label class="form-label" for="form3Example3">Email</label>
+                                            </div>
+
+                                            <div class="form-outline mb-4">
+                                                <input type="password" name="wachtwoord" class="form-control form-control-lg" placeholder="Enter password" />
+                                                <label class="form-label" for="form3Example4">wachtwoord</label>
+                                            </div>
+
+                                            <div class="text-center pt-1 mb-5 pb-1">
+                                                <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" input type="submit" id="submit"> Log
+                                                    in</button>
+                                            </div>
+
+
+                                        </form>
+
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
+                                    <div class="text-white px-3 py-4 p-md-5 mx-md-4">
+                                        <h4 class="mb-4">welkom terug</h4>
+                                        <p class="small mb-0">welkom terug </p>
+                                    </div>
+    </form>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </section>
+</body>
+
+</html>
+
+
+<?php
+
+require_once "../vendor/autoload.php";
+
+use Controllers\DB;
+
+DB::connect();
+if ($_SERVER["REQUEST_METHOD"] === "POST" && $_POST['email'] !== null && $_POST['wachtwoord'] !== null) {
+
+    $user = DB::select('users', ['email' => $_POST['email'], 'wachtwoord' => $_POST['wachtwoord']]);
+
+    if ($user && $user[0]["active"] == 1) {
+        session_start();
+        $_SESSION['userId'] = $user[0]["id"];
+        $_SESSION['role'] = $user[0]["role"];
+        header("location:home.php");
+    } else {
+        echo '<script>alert("uw inlog gegevens kloppen niet.")</script>';
+    }
+}
+
+
+?>
--- Pagina/nieuwe.php
+++ Pagina/nieuwe.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Accommodatieoverzicht</title>
     <link rel="stylesheet" href="../Styles/css.css">
 </head>
+
 <body>
     <div class="container">
         <h1>Accommodatieoverzicht</h1>
@@ -14,25 +16,38 @@
         $username = 'root';
         $password = '';
 
+
+        $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
+
         try {
-            $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+            $pdo = new PDO($dsn, $username, $password);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-            // Query uitvoeren
-            $stmt = $pdo->query("SELECT * FROM accommodation");
-
-            // Resultaten weergeven
-            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                echo '<div class="accommodatie">';
-                echo '<h2>' . $row['name'] . '</h2>';
-                echo '<p>' . $row['description'] . '</p>';
-                echo '<p>Prijs per nacht: €' . $row['price'] . '</p>';
-                echo '</div>';
-            }
         } catch (PDOException $e) {
-            echo "Verbindingsfout: " . $e->getMessage();
+            die("Fout bij verbinding met de database: " . $e->getMessage());
+        }
+
+        // Query uitvoeren om accommodaties op te halen
+        $query = "SELECT * FROM accommodation INNER JOIN photo on accommodation.photo";
+        $stmt = $pdo->query($query);
+
+        // Accommodaties weergeven
+        while ($row = $stmt->fetch(PDO::FETCH_CLASS)) {
+            $image = $row['image'];
+            $name = $row['name'];
+            $price = $row['price'];
+            $description = $row['description'];
+
+            echo '<div class="accommodation">';
+            echo '<div class="image"><img src="' . $image . '"></div>';
+            echo '<div class="info">';
+            echo '<h2>' . $name . '</h2>';
+            echo '<p>Prijs: ' . $price . '</p>';
+            echo '<p>' . $description . '</p>';
+            echo '</div>';
+            echo '</div>';
         }
         ?>
     </div>
 </body>
+
 </html>
