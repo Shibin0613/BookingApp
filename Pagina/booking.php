b5d6dd77e626603f8c82326f4a0414cdc7a168b2
Timhamer <240159@edu.rocfriesepoort.nl>
2023-05-30 12:03:23 +0200

    Booking pagina update en API naar funciton

--- Pagina/booking.php
+++ Pagina/booking.php
@@ -1,4 +1,8 @@
-<?php include "header.php"; ?>
+<?php include "header.php"; 
+include "../Functions/services.php"; 
+
+$service = new Services();
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,6 +15,13 @@
 </head>
 
 <body>
+  
+    <div class="form-group">
+    <label for="postcode">Categorie</label>
+  </div>
+  <div class="form-group">
+    <label for="huisnummer">Accommodatie</label>
+  </div>
 <form method="POST" action="">
   <div class="form-group">
     <label for="postcode">Postcode</label>
@@ -18,70 +29,22 @@
   </div>
   <div class="form-group">
     <label for="huisnummer">Huisnummer</label>
-    <input type="text" class="form-control" id="huisnummer" name="huisnummer" required>
+    <input type="text" class="form-control" id="huisnummer" name="huisnummer"  required>
   </div>
   <button name ="checkadres">Check adres</button>
   <?php
-if(isset($_POST['checkadres'])){
-  $postcode = $_POST['postcode'];
-  $huisnummer = $_POST['huisnummer'];
-  // Set up your Kadaster API credentials
-$clientId = 'YOUR_CLIENT_ID'; // Replace with your actual client ID
-$clientSecret = 'YOUR_CLIENT_SECRET'; // Replace with your actual client secret
-
-// Create a request URL
-$requestUrl = 'https://geodata.nationaalgeoregister.nl/locatieserver/v3/free?q=' . urlencode($postcode) . '&fq=type:adres&rows=1';
-
-// Set up the cURL request
-$curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, $requestUrl);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-    'Content-Type: application/json',
-    'Authorization: Basic ' . base64_encode($clientId . ':' . $clientSecret)
-));
-
-// Send the request
-$response = curl_exec($curl);
-
-// Parse the JSON response
-$data = json_decode($response, true);
-
-// Check if the response contains any addresses
-if (!empty($data['response']['docs'])) {
-    // Retrieve the first address
-    $address = $data['response']['docs'][0];
-
-    // Extract relevant address components
-    $straat = $address['straatnaam'];
-    $woonplaats = $address['woonplaatsnaam'];
-
-    // Output the address
-    echo"
-    <div class='form-group'>
-    <label for='adres'>Adres</label>
-    <input type='text' class='form-control' id='adres' name='adres' value='".$straat." ".$huisnummer."' readonly>
-    </div>
-    <div class='form-group'>
-    <label for='woonplaats'>Woonplaats</label>
-    <input type='text' class='form-control' id='woonplaats' name='woonplaats' value='".$woonplaats."' readonly>
-    </div>
-    ";
-}else{
-    echo "<script>alert('De postcode bestaat niet')</script>";
-}
-}
+$inputValues = $service->addressAPI();
 ?>
 </form>
 <form method="POST" action="../Handlers/bookingHandler.php">
-  <input hidden name='postcode' value='<?php echo $postcode;?>'>
-  <input hidden name='huisnummer' value='<?php echo $huisnummer;?>'>
-  <input hidden name='woonplaats' value='<?php echo $woonplaats;?>'>
+  <input hidden name='postcode' value='<?= $inputValues['postcode']?>'>
+  <input hidden name='huisnummer' value='<?= $inputValues['huisnummer']?>'>
+  <input hidden name='woonplaats' value='<?= $inputValues['woonplaats']?>'>
   <div class="form-group">
     <label for="naam">Naam</label>
     <input type="text" class="form-control" id="naam" name="naam" required>
   </div>
-  <input type="hidden" name="datum" type="date" value="<?php echo date("Y-m-d"); ?>">
+  <input type="hidden" name="datum" type="date" value="<?= date("Y-m-d"); ?>">
   <div class="form-group">
     <label for="achternaam">Achternaam</label>
     <input type="text" class="form-control" id="achternaam" name="achternaam" required>
