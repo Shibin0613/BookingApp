e67e65f7d58183a91d2c246bfd89d8bb743dd1f2
ShibinPan <220617@edu.rocfriesepoort.nl>
2023-06-08 15:05:38 +0200

    boeking beter gemaakt

--- Pagina/booking.php
+++ Pagina/booking.php
@@ -37,6 +37,7 @@ $accommodationId = $_GET['id'];
     </div>
     <button name="checkadres">Check adres</button>
     <?php
+    //hier wordt de functie geroepen, en met ingevulde postcode en huisnummer checkt hij vanuit de api of het bestaat, en wordt het gereturnd, met adres bv
     $inputValues = $service->addressAPI();
     ?>
     
@@ -71,24 +72,24 @@ $accommodationId = $_GET['id'];
     </div>
     <div class="form-group">
       <label for="18+">18+ jaar</label>
-      <input type="number" class="form-control" id="inputNumber1" name="number1" value="1" min="1" max="10">
+      <input type="number" class="form-control" id="inputNumber1" name="number1" value="1" min="1" max="5">
     </div>
     <div class="form-group">
       <label for="4-18">4-18 jaar</label>
-      <input type="number" class="form-control" id="inputNumber2" name="number2" value="0" min="0" max="10">
+      <input type="number" class="form-control" id="inputNumber2" name="number2" value="0" min="0" max="5">
     </div>
     <div class="form-group">
       <label for="0-4">0-4 jaar</label>
-      <input type="number" class="form-control" id="inputNumber3" name="number3" value="0" min="0" max="10">
+      <input type="number" class="form-control" id="inputNumber3" name="number3" value="0" min="0" max="5">
     </div>
     <div id="result"></div><br>
     <div class="form-group">
       <label for="date">Checkin datum</label>
-      <input type="date" class="form-control" id="date" name="checkindate" value="<?php echo date("Y-m-d", strtotime('+5 days')); ?>">
+      <input type="date" class="form-control" id="checkindate" name="checkindate" value="<?php echo date("Y-m-d", strtotime('+5 days')); ?>">
     </div>
     <div class="form-group">
       <label for="date">Checkout datum</label>
-      <input type="date" class="form-control" id="datePickerId" name="checkoutdate" value="<?php echo date("Y-m-d", strtotime('+10 days')); ?>">
+      <input type="date" class="form-control" id="checkoutdate" name="checkoutdate" value="<?php echo date("Y-m-d", strtotime('+10 days')); ?>">
     </div>
     <input hidden id="resultInput">
     <script>
@@ -131,16 +132,27 @@ $accommodationId = $_GET['id'];
   
 </script>
 <script>
+  //checkindatum niet selecteerbaar binnen de aankomende 5 dagen
   var today = new Date();
-  today.setDate(today.getDate() + 10); // Add five days to the current date
+  today.setDate(today.getDate() + 5); //Voeg 5 dagen met de datum van vandaag
 
   var year = today.getFullYear();
   var month = String(today.getMonth() + 1).padStart(2, '0');
   var day = String(today.getDate()).padStart(2, '0');
   var datePattern = year + '-' + month + '-' + day;
 
-  document.getElementById("datePickerId").value = datePattern;
-  document.getElementById("datePickerId").min = datePattern;
+  document.getElementById("checkindate").value = datePattern;
+  document.getElementById("checkindate").min = datePattern;
+
+  today.setDate(today.getDate()+3); //Voeg 3 dagen met de datum van vandaag
+
+  var year = today.getFullYear();
+  var month = String(today.getMonth() + 1).padStart(2, '0');
+  var day = String(today.getDate()).padStart(2, '0');
+  var datePattern = year + '-' + month + '-' + day;
+
+  document.getElementById("checkoutdate").value = datePattern;
+  document.getElementById("checkoutdate").min = datePattern;
 </script>
 
 </body>
